5613450de9550a234208282c5a28f6fb56bea93b
waikyjung <waiky.jung@gmail.com>
2024-10-22 18:31:08 +0000

    update

--- app.rb
+++ app.rb
@@ -4,5 +4,18 @@ require "sinatra/reloader"
 set :public_folder, __dir__ + '/static'
 
 get("/") do
+  redirect "/search"
+end
+
+get("/search") do
   erb(:search)
+end
+
+get("/sales") do
+  @store = params.fetch("store")
+  @lowerprice = params.fetch("lowerprice")
+  @upperprice = params.fetch("upperprice")
+  
+
+  erb(:sales)
 end
