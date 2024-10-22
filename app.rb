a94c5302ded1b70a7e4409939eab28568250ed94
waikyjung <waiky.jung@gmail.com>
2024-10-22 18:51:40 +0000

    update

--- app.rb
+++ app.rb
@@ -15,7 +15,9 @@ get("/sales") do
   @store = params.fetch("store")
   @lowerprice = params.fetch("lowerprice")
   @upperprice = params.fetch("upperprice")
-  
+  @metacritic = params.fetch("metacritic")
+  @aaa = params.fetch("AAA")
+  @title = params.fetch("title")
 
   erb(:sales)
 end
