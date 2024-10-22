a15d230d39e34fd011054ba6f34857c0d5dc2a3e
waikyjung <waiky.jung@gmail.com>
2024-10-22 21:11:46 +0000

    update

--- app.rb
+++ app.rb
@@ -1,10 +1,12 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
 set :public_folder, __dir__ + '/static'
 
 get("/") do
-  redirect "/search"
+  redirect("/search")
 end
 
 get("/search") do
@@ -12,12 +14,31 @@ get("/search") do
 end
 
 get("/sales") do
-  @store = params.fetch("store")
-  @lowerprice = params.fetch("lowerprice")
-  @upperprice = params.fetch("upperprice")
-  @metacritic = params.fetch("metacritic")
-  @aaa = params.fetch("AAA")
-  @title = params.fetch("title")
+  cheapshark_http = "https://www.cheapshark.com/api/1.0/deals?"
+  
+  store = params.fetch("store")
+  lowerprice = params.fetch("lowerprice")
+  upperprice = params.fetch("upperprice")
+  metacritic = params.fetch("metacritic")
+  aaa = params.fetch("AAA")
+  title = params.fetch("title")
+  page ||= params.fetch("pagenumber")
+  
+  case store
+  when "Steam"
+    @store_code = 1
+  when "GOG"
+    @store_code = 7
+  when "Green Man Gaming"
+    @store_code = 3
+  end
+  
+  request = "#{cheapshark_http}&storeID=#{store}&lowerPrice=#{lowerprice}&upperPrice=#{upperprice}&metacritic=#{metacritic}&AAA=#{aaa}&pageNumber=0"
+  raw_response = HTTP.get(request)
+  parsed_response = JSON.parse(raw_response)
+
+  system("clear")
+
 
   erb(:sales)
 end
--- test.rb
+++ test.rb
@@ -1,13 +1,33 @@
 require "http"
+require "json"
 
-store_http = "https://www.cheapshark.com/api/1.0/stores"
-raw_response = HTTP.get(store_http)
-raw_response_parsed = JSON.parse(raw_response)
-raw_response_parsed.each do |x|
-  data = ""
-  data = x.fetch("storeID").to_s + "|" + x.fetch("storeName").to_s + "|" + x.fetch("isActive").to_s
-  y = x.fetch("images")
-  data = data + "|" + y.fetch("banner") + "|" + y.fetch("logo") + "|" + y.fetch("icon")
-  pp data
+# store_http = "https://www.cheapshark.com/api/1.0/stores"
+# raw_response = HTTP.get(store_http)
+# raw_response_parsed = JSON.parse(raw_response)
+# raw_response_parsed.each do |x|
+#   data = ""
+#   data = x.fetch("storeID").to_s + "|" + x.fetch("storeName").to_s + "|" + x.fetch("isActive").to_s
+#   y = x.fetch("images")
+#   data = data + "|" + y.fetch("banner") + "|" + y.fetch("logo") + "|" + y.fetch("icon")
+#   pp data
+# end
+
+# ----------------------------------------
+
+# cheapshark_http = "https://www.cheapshark.com/api/1.0/deals?"
   
-end
+# store = 1
+# lowerprice = 0
+# upperprice = 15
+# metacritic = 80
+# aaa = 0
+# title = ""
+# request = "#{cheapshark_http}&storeID=#{store}&lowerPrice=#{lowerprice}&upperPrice=#{upperprice}&metacritic=#{metacritic}&AAA=#{aaa}&pageNumber=0"
+# raw_response = HTTP.get(request)
+# parsed_response = JSON.parse(raw_response)
+
+# system("clear")
+
+# parsed_response.each do |x|
+#   pp x.fetch("title") + " | " + x.fetch("salePrice").to_s
+# end
